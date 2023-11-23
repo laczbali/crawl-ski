5c3130cec3280e7bf11f22ff4756fd4b97391b5e
Balázs Laczkó <laczbali@gmail.com>
2023-11-23 21:19:20 +0100

    adding docker setup

---
+++ deploy/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+cd /app
+
+git clone https://github.com/laczbali/crawl-ski
+cd crawl-ski
+npm install
+npx tsc
+cd ..
+
+node crawl-ski/bin/index.js $1 #  pass 'debug' as first param to enable debug mode
+
+rm -rf crawl-ski
