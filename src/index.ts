4095aae80d2b16935e73552ce7bac729fe21e4d3
Balázs Laczkó <laczbali@gmail.com>
2023-11-23 19:05:37 +0100

    adjusting debug mode

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Orchestrator } from "./CrawlBase/Orchestrator.js";
 import { CrawlIgluski } from "./CrawlIgluski/CrawlIgluski.js";
 
+var crawlDelayMs = 1000;
+var isDebugMode = process.argv[2] === 'debug';
+
 await Orchestrator.doAll(
     '.\\output',
-    new CrawlIgluski()
+    new CrawlIgluski(crawlDelayMs, isDebugMode)
 );
