649c8bfc767412ff8f929b10d13d560cf56bb4f4
LaczkóBalázs <lab@frankly.dk>
2023-11-29 18:58:47 +0100

    fixing csv filepath

--- src/CrawlBase/CsvHandler.ts
+++ src/CrawlBase/CsvHandler.ts
@@ -1,6 +1,7 @@
 import { mkdirp } from "mkdirp";
 import { Resort } from "./Models/Resort.js";
 import * as fs from 'fs';
+import path from "path";
 
 export class CsvHandler {
     public static async dumpResorts(resorts: Resort[], outputDir: string): Promise<void> {
@@ -19,7 +20,9 @@ export class CsvHandler {
             var headerData = sheetInfo.headers.join(';');
             var sheetData = resortsAsCsvData[sheetIndex].map(x => x.join(';')).join('\n');
             var sheet = `${headerData}\n${sheetData}`;
-            fs.writeFile(`${outputDir}\\${sheetInfo.name}.csv`, sheet, (err) => { if (err) console.error(err); });
+
+            var filePath = path.join(outputDir, `${sheetInfo.name}.csv`);
+            fs.writeFile(filePath, sheet, (err) => { if (err) console.error(err); });
         });
     }
 }
